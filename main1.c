9f4fd73fefea9a4fb435e0b63b859068d4c335c0
Hugebob1 <01197882@pw.edu.pl>
2025-04-02 13:06:16 +0200

    dodanie zapisu podgrafow do jedngeo pliku
    # Conflicts:
    #	graph_original.csv
    #	main1.c

--- main1.c
+++ main1.c
@@ -23,56 +23,25 @@ int main(int argc, char **argv){
 
     //domyslna wartosc x = 0
     int x = 0;
-    int opt;
-    int numParts = 2;
-    char binaryname[100]="subgraphs.bin";
-    char tekstowy[100]="subgraphs.txt";
-    float maxDiff = 10.0f;
-    while ((opt = getopt(argc, argv, "hb:a:g:p:d:f")) != -1) { //a.exe graf.txt -g 1 -p 2 -d 60 (w skrócie flaga argument, flaga argument_
-        switch (opt) {
-            case 'h':
-                printf("Instrukcja programu\n");
-            return 0;
-          case 'b':
-                strcpy(binaryname, optarg);  // Przypisanie nazwy pliku binarnego
-            break;
-            case 'a':
-                strcpy(tekstowy, optarg);  // Przypisanie nazwy pliku tekstowego
-            break;
-            case 'g':
-                if (optarg) {
-                    x = atof(optarg);
-                }
-            break;
-            case 'p':
-                if (optarg) {
-                    numParts = atof(optarg);
-                }
-            break;
-            case 'd':
-                if (optarg) {
-                    maxDiff = atof(optarg);
-                }
-            break;
-            case 'f':
-                break;
-            default:
-                printf("Wpisana flaga nie istnieje\n");
-            return 1;
-        }
-    }
-   // printf("%s",argv[optind-1]);
-    GraphChunk graph = addEdges(argv[optind], x);
+
+    GraphChunk graph = addEdges(argv[1], x);
 
     exportGraph(graph, "graph_original.csv");
 
     validateGraphChunk(graph);
 
     isGraphConnected(graph);
-    GraphChunk* parts = splitGraphGreedyBalanced(graph, numParts, maxDiff);
 
-    saveSubGraphs(parts, numParts, tekstowy);
-    saveSubGraphsCompactBinary(parts,numParts, binaryname);
+    saveGraphBinaryCompact(graph, "graph_original.bin");
+
+    GraphChunk* parts = splitGraphGreedyBalanced(graph, 2, 100.0f);
+
+    for(int i = 0; i < 2; i++) {
+        char filename[64];
+        sprintf(filename, "graph_part%d.csv", i);
+        exportGraph(parts[i], filename);
+    }
+
     isGraphConnected(parts[0]);
     isGraphConnected(parts[1]);
 
@@ -83,8 +52,8 @@ int main(int argc, char **argv){
 
     printf("Czas dzialania: %.6f sekund\n", time_spent);
 
-    printf("Rozmiar pliku csv: %ld KB\n", getFileSize(tekstowy)/1024);
-    printf("Rozmiar pliku bin: %ld KB\n", getFileSize(binaryname)/1024);
+    printf("Rozmiar pliku csv: %ld KB\n", getFileSize("graph_original.csv")/1024);
+    printf("Rozmiar pliku bin: %ld KB\n", getFileSize("graph_original.bin")/1024);
 
     return 0;
 }
